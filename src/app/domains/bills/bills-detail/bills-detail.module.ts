dca7d358d1c1f37641dd82c20f9912a35de2af11
Santiago Ojeda Botan <sojedabo@emeal.nttdata.com>
2023-03-08 18:07:52 +0100

    added trasnlate modules fot build component

--- src/app/domains/bills/bills-detail/bills-detail.module.ts
+++ src/app/domains/bills/bills-detail/bills-detail.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BillsDetailComponent } from './bills-detail.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
 @NgModule({
   declarations: [BillsDetailComponent],
   imports: [
-    CommonModule,    
+    CommonModule,
+    TranslateModule
   ]
 })
 export class BillsDetailModule { }
