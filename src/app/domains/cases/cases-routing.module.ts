ed7cd362534e5e202c50d76d8b111129b514bb04
Santiago Ojeda Botan <sojedabo@emeal.nttdata.com>
2023-03-09 10:49:43 +0100

    update navigation

--- src/app/domains/cases/cases-routing.module.ts
+++ src/app/domains/cases/cases-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CaseDetailComponent } from './cases-components/case-detail/case-detail.component';
+import { CasesComponent } from './cases.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
+
       {
+        path: '',
+        component: CasesComponent
+      }, {
         path: 'detail',
         component: CaseDetailComponent
       }
